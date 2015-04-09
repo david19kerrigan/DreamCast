85c8566911a7b4d8d18b4013d7f48a7e94b087f6
blehw <winton2095@gmail.com>
2015-04-08 20:02:41 -0400

    W

---
+++ Tile.java
@@ -0,0 +1,17 @@
+public class Tile {
+
+    private String type;
+
+    public Tile() {
+	type = "grass";
+    }
+
+    public String getType() {
+	return type;
+    }
+
+    public void setType(String data) {
+	type = data;
+    }
+
+}
