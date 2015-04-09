2b2296796bbeb61c83656a8435e8775f1fb30df1
JacobHoffman <jackstudent149@gmail.com>
2015-04-08 20:28:44 -0400

    heehee

---
+++ Characters/Winston.java
@@ -0,0 +1,17 @@
+public class Winston{
+    private Arraylist[] items = new ArrayList;
+    private int speed;
+    private int range;
+    private int heavyweapon;
+    private int lightweapon;
+    private int healingmagic;
+    private int fightingmagic;
+    private int alchemy;
+    private int lightarmor;
+    private int heavyarmor;
+    private int luck;
+    private int speech;
+    private int stealth;
+    private int lockpicking;
+    private int charm;
+    
---
+++ itemmap.java
@@ -0,0 +1,5 @@
+import java.util.*;
+import java.io.*;
+public class itemmap{
+    ArrayList[][] itemmap = new ArrayList[][];
+}
