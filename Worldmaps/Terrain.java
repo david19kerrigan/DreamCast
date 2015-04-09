3ecd45c68bfda69077aa4d2a5e385019f3f0029a
blehw <winton2095@gmail.com>
2015-04-08 22:54:04 -0400

    commited terrain stuff

--- Worldmaps/Terrain.java
+++ Worldmaps/Terrain.java
@@ -21,21 +21,57 @@ public class Terrain {
     }
 
     public void randomize() {
+	/*
 	Random rand = new Random();
-	for (int i=0;i<maxY;i++) {
-	    for (int k=0;k<maxX;k++) {
-		if (rand.nextInt(100) > 30) {
-		    for (int y=i;y<20;y++) {
-			for (int z=k;z<20;z++) {
+	int[][] arr = new int[2][5];
+	for (int b=0;b<2;b++) {
+	    for (int a=0;a<5;a++) {
+		if (rand.nextInt(100) < 30) {
+		    arr[b][a] = 1; 
+		    System.out.println(1);
+		} else {
+		    arr[b][a] = 0;
+		    System.out.println(0);
+		}
+	    }
+	    System.out.println("space");
+	}
+	int xCounter = 0;
+	int yCounter = 0;
+	while (yCounter<2) {
+	    while (xCounter<5) {
+		yCounter++;
+		for (int y=(yCounter*10);y<y+20;y++) {
+		    for (int z=(xCounter*10);z<z+20;z++) {
+			terrain[y][z].setType("water");
+			terrain[y][z].setImage("~");
+		    }
+		}
+	    }
+	    xCounter++;
+	}
+	*/
+       
+	Random rand = new Random();
+	int i = 0;
+	int k = 0;
+	while (i<101) {
+	    while (k<41) {
+		if (rand.nextInt(100) > 0) {
+		    for (int y=i;y<i+20;y++) {
+			for (int z=k;z<k+20;z++) {
 			    terrain[y][z].setType("water");
 			    terrain[y][z].setImage("~");
 			}
 		    }
-		    i = i + 20;
-		    k = k + 20;
-		}	
+		}
+		k=k+20;
 	    }
+	    i=i+20;
+
 	}
+       
+	/*
 	int c = 0;
 	while (c < 1000) {
 	    for (int a=0;a<maxY-1;a++) {
@@ -49,6 +85,7 @@ public class Terrain {
 	    }
 	    c = c + 1;
 	}
+	*/
     }
 
     public String toString() {
