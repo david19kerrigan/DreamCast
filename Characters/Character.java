d9533f53ab9664d3fcfe675a5421503723ebe239
JacobHoffman <jackstudent149@gmail.com>
2015-04-10 09:38:33 -0400

    heehe

--- Characters/Character.java
+++ Characters/Character.java
@@ -1,6 +1,8 @@
+import java.io.*;
+import java.util.*;
 public class Character{
-    private Arraylist[] items = new ArrayList;
-    private int SP;
+    private ArrayList<String> items;
+    private int SPEED;
     private int HP;
     private int ATTH;
     private int ATTL;
@@ -14,66 +16,83 @@ public class Character{
     private int STEALTH;
     private int PICK;
     private int CHARM;
+//item example "1212121212"
  //speed11 health12 heavyweapon13 lightweapon14
-    public Character(int level){
-	items.populate(40, level);
+    public Character(int level, String specialization){
+    	populate(40, level);
+    	while(OP()){
+		ArrayList<String> blank = new ArrayList<String>();
+		items = blank;
+		populate(40, level);
+	}
 	calcstats();
 }
-
+    
+    public static int variabilize(int variable, int current){
+		Random var = new Random();		
+		 current += var.nextInt(variable);
+		 	return current;
+	}
+    
 	public void populate(int amount, int level){
 	while(amount != 0){
 		amount = amount - 1;
 		int type = 1;
-		items.addone(type.variabilize(14), level);
+		addone(variabilize(14, type), level, items);
 	}
 }
-	public String addone(int type , int level){
-	String itemtoadd = ""
-	level.variabilize(3);
+	public static void addone(int type , int level, ArrayList<String> current){
+	String itemtoadd = "";
+	variabilize(3, level);
 	while( level != 0){
 			level--;
 			itemtoadd = itemtoadd + type;
 	}
-	items.add(itemtoadd);
+	current.add(itemtoadd);
 }
 
-	public void variabilize(int variable){
-		Random variable = new Random();		
-		this = this + variable.nextInt(variable);
-	}
 
-	public void poststatcalc(){
-	SP;
-    	HP;
-    ATTH;
-    ATTL;
-    HPM;
-    ATTM;
-    ALC;
-    LDEF;
-    HDEF;
-    LUCK;
-    SPEECH;
-    STEALTH;
-    PICK;
-    private int CHARM;}
+	public void calcstats(){
+	SPEECH = count(11);
+	CHARM = count(12);
+	STEALTH = count(13);
+    	PICK = count(14);
+	ALC = count(15);
+    	ATTH = count (16);
+    	ATTL = count (17);
+	ATTM = count (18);
+	HPM = count (19) ;
+	HP = count(20);
+	LDEF = count (21);
+    	HDEF = count (22);
+    	LUCK = count (23);
+    	SPEED = count (24);
+}
+// attack social
+	public String category( int type ){
+    if( 11 <= type || type <= 15 )
+    	return "social" ;	
+	if ( 16 <= type || type <= 19)
+		return "attack" ;
+	return "not a valid interaction type";
+}  
 
 	public int count( int type){		
 		int place = 0;
 		int counter = 0;
 		while (place != items.size()){
-			counter = counter + item.get(place).countelement(type);
+			counter = counter + countelement(type, items.get(place));
 			place += 1;
 		}
 		return counter;
 }
 
-	public int countelement(int type){
+	public static int countelement(int type, String current){
 		int howmany = 0;
 		String typecheck = "" + type;
 		int place = 0;
-		while( place != length){
-			if ( typecheck.getChar(0) == getChar(place) && typecheck.getChar(1) == getChar(place + 1)){
+		while( place != current.length()){
+			if ( typecheck.charAt(0) == current.charAt(place) && typecheck.charAt(1) == current.charAt(place + 1)){
 				howmany += 1;
 				place += 2;
 			}
@@ -83,18 +102,92 @@ public class Character{
 
 
 			
-	public void statcalcphase(Character opponent){
-		this.poststatcalc();
-		opponent.poststatcalc();
+	public void calcstatsboth(Character opponent){
+		this.calcstats();
+		opponent.calcstats();
 }
 
-    public int interact( Characer ai, int type){
-	if( cateory(type) == "attack"){ attack(opponent);}
-	if( category(type) == "social"){ social(opponent);}
-	if (category(type) =="gift" ){ gift(opponent);}
-	if (category(type) == "craft"){craft(opponent)}
-    }
-    public String category{ int type }
-    if type == 
-		
-		
+    public String interact( Character ai, int type){
+	if( category(type) == "attack"){ return attack(ai, type);}
+	if( category(type) == "social"){return social(ai, type);}
+		else{return "not an interaction";}
+}
+//SPEECH = count(11)CHARM = count(12)STEALTH = count(13)PICK = count(14)ALC = count(15)ATTH = count (16)ATTL = count (17)ATTM = count (18)HPM = count (19)HP = count(20)LDEF = count (21)HDEF = count (22)LUCK = count (23)SPEED = count (24)
+	public String attack( Character ai, int type){
+		String finalHP = "";
+		ai.HP = ai.HP - count(type)/(ai.LDEF + ai.HDEF);
+		finalHP += ai.HP;
+		return finalHP;
+}
+ 
+	public String social( Character ai, int type){
+	if (type == 11 && variabilize(100, 0) <= SPEECH  )
+		return "+11";
+	else if (type == 11)
+		return "-11";
+	if (type == 11 && variabilize(100, 0) <= CHARM  )
+		return "+12";
+	else if (type == 11)
+		return "-12";
+	if (type == 11 && variabilize(100, 0) <= STEALTH  )
+		return "+13";
+	else if (type == 11)
+		return "-13";
+	if (type == 11 && variabilize(100, 0) <= SPEECH  )
+		return "+14";
+	else if (type == 11)
+		return "-14";
+	return "0";
+}
+
+	public String deathitem(){
+		return items.get(variabilize(40, 0));
+}
+	public String battlereward(Character ai){
+		Scanner input = new Scanner(System.in);
+		ArrayList<String> Storage = new ArrayList<String>();
+		Storage = items;
+		System.out.println("which of your items would you like to replace?");
+		int index = input.nextInt();
+		String deathitem = ai.deathitem();
+		items.add(index + 1, deathitem);
+		items.remove(index);
+		if(OP()){ 
+			items = Storage;
+			battlereward(ai);
+		calcstats();
+		}
+		return deathitem;
+}
+
+	public void battle(Character ai){
+		Scanner typein = new Scanner(System.in);
+		int type = typein.nextInt();
+		while( ai.HP != 0 && HP != 0){
+				System.out.println(" time to attack");				
+				interact(ai, type);
+				ai.interact(this, type);
+		}
+		if (HP != 0)
+			battlereward(ai);
+}
+
+	public boolean OP(){
+	int i = 10;
+	int low = count(i);
+	int high = 0;
+		while(i != 24){
+			if (count(i) < low)
+				low = count(i);
+			if (count(i) > high)	
+				high = count(i); 
+				i = i + 1;
+			}
+		if ((high / low) > 10)	
+			return true;	
+		return false;
+}
+	public static void main(String[]args){
+}
+}
+
